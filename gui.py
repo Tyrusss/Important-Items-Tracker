a544203647cbe23e84cefa960f895387b6bc46cf
Tyrus <42982810+Tyrusss@users.noreply.github.com>
2019-01-18 19:34:42 +0000

    ice is dead

---
+++ gui.py
@@ -0,0 +1,27 @@
+from tkinter import (PhotoImage, Tk, Button, RIGHT, LEFT, TOP, BOTTOM)
+
+root=Tk()
+a=Button(root,justify = RIGHT)
+root.title("Tyrus's important items tracker")
+
+x = 'rupee/rupee_green.gif'
+y = 'ice/ice0.gif'
+
+def rupeeimage():
+    global x
+    if x == 'rupee/rupee_grey.gif':
+        x= 'rupee/rupee_green.gif'
+    elif x == 'rupee/rupee_green.gif':
+        x= 'rupee/rupee_grey.gif'
+
+
+def clicky():
+    rupeeimage()
+
+    rupeephoto = PhotoImage(file=x)
+    a.config(bg='black',activebackground='black',image=rupeephoto,width="224",height="351",command= clicky)
+    a.pack(side=RIGHT)
+
+    root.mainloop()
+
+clicky()
